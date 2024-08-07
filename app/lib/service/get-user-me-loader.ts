98cfdca2d53ff35ff6ed3dfa98599e7a968a8275
microcow <160401555+microcow@users.noreply.github.com>
2024-08-07 09:34:31 +0900

    Update get-user-me-loader.ts

--- app/lib/service/get-user-me-loader.ts
+++ app/lib/service/get-user-me-loader.ts
@@ -8,7 +8,7 @@ export async function getUserMeLoader() {
   console.log('authToken', authToken);
   try {
     const response = await fetch("http://localhost:8080/api/users/me", {
-      method: "GET",
+      method: "GET", //
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${authToken}`,
